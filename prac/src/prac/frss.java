c8db45672ce3d5c0f32dadb9082d229834088f16
ssuta <ssuta@TUFGAMING-SHUBHAM-18>
2023-05-03 18:01:09 +0530

    second commit

--- prac/src/prac/frss.java
+++ prac/src/prac/frss.java
@@ -5,6 +5,12 @@ public class frss {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("holaaaaa");
+		System.out.println("holaaaaa");
+		System.out.println("holaaaaa");
+		
+		System.out.println("holaaaaa");
+		System.out.println("holaaaaa");
+		System.out.println("holaaaaa");
 
 	}
 
